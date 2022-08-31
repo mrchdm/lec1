efec441147eeb2227e86071029e7da8ceb5f9b42
mrchdm <dementevamm@yandex.ru>
2022-08-31 15:30:27 +0300

    add condition

---
+++ example005_condition_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+{
+    Console.WriteLine("Ура это же Маша");
+}
+else
+{
+Console.Write("привет ");
+Console.WriteLine(username);
+}
